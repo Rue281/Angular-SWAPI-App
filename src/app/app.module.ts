f52c999b5c6fb5294b9a277a60df768ddf177ac0
rue281 <rue281@gmail.com>
2020-08-19 22:57:13 +0200

    Added the refactored form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,18 @@ import { SwapiService } from "./services/swapi.service";
 import { DragDropModule } from "@angular/cdk/drag-drop";
 import { DropListComponent } from "./drop-list/drop-list.component";
 import { ReactiveFormsModule } from "@angular/forms";
+import { FormComponent } from './form/form.component';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
-  declarations: [AppComponent, DropListComponent],
+  declarations: [AppComponent, DropListComponent, FormComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     DragDropModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [SwapiService],
   bootstrap: [AppComponent]
